05c660c57239040d28017dc55e3700cb157cef9d
taofikarianto <taofikarianto@gmail.com>
2023-05-18 00:28:09 +0700

    JS Syntax

---
+++ Syntax/script.js
@@ -0,0 +1,58 @@
+// JavaScript syntax is the set of rules, how JavaScript programs are constructed:
+// How to create variables
+var x;
+let y;
+
+// How to use variables
+x = 5;
+y = 6;
+
+let z = x + y;
+console.log(z);
+
+// The JavaScript syntax defines two types of values:
+
+// Fixed values
+// Variable values
+// Fixed values are called Literals.
+
+// Variable values are called Variables.
+
+// Javascript literals
+// The two most important syntax rules for fixed values are
+// 1. Numbers are written with or without decimals:
+
+let numberOne = 9.5;
+let numberTwo = 95;
+
+// 2. String are text written within double or single quotes
+let firstName = "Taofik";
+let lastName = "Arianto";
+
+// Javascript variables
+// In a programming language, variables are used to store data values.
+
+// JavaScript uses the keywords var, let and const to declare variables.
+
+// An equal sign is used to assign values to variables.
+
+// In this example, x is defined as a variable. Then, data is assigned (given) the value 10:
+
+let data;
+data = 10;
+
+// Javascript Operators
+// JavaScript uses arithmetic operators ( + - * / ) to compute values:
+let arithmetic = (2 + 2) * 5;
+
+// Javascript Expressions
+// An expression is a combination of values, variables, and operators, which computes to a value
+// The computation is called an evaluation
+// for example, 5 * 5 evaluates to 25
+let numberFive = 5;
+let jsExpression = numberFive * 5;
+
+// The values can be of various types, such as numbers and strings.
+
+// For example, "John" + " " + "Doe", evaluates to "John Doe":
+let fullName = "Mata" + "" + "Ringan";
